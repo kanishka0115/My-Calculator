6730b1c9f22bcc35ac0b16ae43b534c1a71c7fb0
kanishka0115 <kanishkatomar7810@gmail.com>
2022-10-17 12:13:57 +0530

    created object and called all functions

--- src/MyCalcPkg/MainCalculator.java
+++ src/MyCalcPkg/MainCalculator.java
@@ -4,5 +4,10 @@ public class MainCalculator {
 	public static void main(String [] args) {
 		Calculator c1 = new Calculator();
 		System.out.println(c1.addition(2,4));
+		System.out.println(c1.subtraction(2,4));
+		System.out.println(c1.multiplication(2,4));
+		System.out.println(c1.division(2,4));
+
+		
 	}
 }
